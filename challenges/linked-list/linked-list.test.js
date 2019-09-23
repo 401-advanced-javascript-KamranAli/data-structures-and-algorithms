5c0b4b415d06837b9d3bdaca72556a009d934530
Kamran Ali <kgali21@gmail.com>
2019-09-23 13:18:56 -0700

    linked lst challenge complete

--- challenges/linked-list/linked-list.test.js
+++ challenges/linked-list/linked-list.test.js
@@ -20,17 +20,16 @@ describe('linked lists', () => {
   });
 
   it('node exists in the list', () => {
-    const list = new LinkList();
+    const list = new LinkList;
     list.insert('node1');
     expect(list.includes(list.head.num)).toBe(true);
   });
 
   it('returns a string of all the values in the linked list', () => {
-    const list = new LinkList();
+    const list = new LinkList;
     list.insert('node1');
-    const stuff = list.head.num;
-    expect(list.toString(stuff)).toEqual(String(stuff));
-
+    const result = list.head.num;
+    expect(list.head.num.toString()).toBe(result);
   });
 
 });
