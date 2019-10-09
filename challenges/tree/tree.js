8d325366ab3bb21b1c72487762d01fa02625c105
Kamran Ali <kgali21@gmail.com>
2019-10-08 17:01:04 -0700

    everything set up, up to preorder just need to do 2 more methods

--- challenges/tree/tree.js
+++ challenges/tree/tree.js
@@ -11,15 +11,57 @@ class BinaryTree {
     this.root = null;
   }
 
-  insert(value) {
+  add(value) {
     const newNode = new Node(value);
     if(this.root === null) {
-      this.root = newNode;
+      return this.root = newNode;
     } else {
       this.insertNode(this.root, newNode);
     }
   }
+
+  insertNode(node, newNode) {
+    if(newNode.value < node.value) {
+      if(node.left === null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      if(node.right === null) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
+
+  contains(node, value) {
+    if(node === null) {
+      return false;
+    } else if(value < node.value) {
+      return this.contains(node.left, value);
+    } else if(value > node.value) {
+      return this.contains(node.right, value);
+    }
+    return true;
+  }
+
+  preOrder() {
+    return preOrderHelp(this.root, []);
+  }
+
 }
+
+function preOrderHelp(node, result) {
+  if(node) {
+    result.push(node.value);
+    if(node.left) preOrderHelp(node.left, result);
+    if(node.right) preOrderHelp(node.right, result);
+  }
+  return result;
+}
+
 
 module.exports = {
   Node,
